3f934f354af1215193f100068301e571a1418010
praneshjayasundar <praneshgunner@gmail.com>
2014-07-01 21:39:14 +0530

    Create login-exec.php

---
+++ login-exec.php
@@ -0,0 +1,81 @@
+<?php
+	//Start session
+	session_start();
+	
+	//Include database connection details
+	require_once('config.php');
+	
+	//Array to store validation errors
+	$errmsg_arr = array();
+	
+	//Validation error flag
+	$errflag = false;
+	
+	//Connect to mysql server
+	$link = mysql_connect(DB_HOST, DB_USER, DB_PASSWORD);
+	if(!$link) {
+		die('Failed to connect to server: ' . mysql_error());
+	}
+	
+	//Select database
+	$db = mysql_select_db(DB_DATABASE);
+	if(!$db) {
+		die("Unable to select database");
+	}
+	
+	//Function to sanitize values received from the form. Prevents SQL injection
+	function clean($str) {
+		$str = @trim($str);
+		if(get_magic_quotes_gpc()) {
+			$str = stripslashes($str);
+		}
+		return mysql_real_escape_string($str);
+	}
+	
+	//Sanitize the POST values
+	$roll = clean($_POST['roll']);
+	$password = clean($_POST['password']);
+	
+	//Input Validations
+	if($roll == '') {
+		$errmsg_arr[] = 'Roll Number missing';
+		$errflag = true;
+	}
+	if($password == '') {
+		$errmsg_arr[] = 'Password missing';
+		$errflag = true;
+	}
+	
+	//If there are input validations, redirect back to the login form
+	if($errflag) {
+		$_SESSION['ERRMSG_ARR'] = $errmsg_arr;
+		session_write_close();
+		header("location: login-form.php");
+		exit();
+	}
+	
+	//Create query
+	$qry="SELECT * FROM members WHERE RollNumber='$roll' AND password='$password'";
+	$result=mysql_query($qry);
+	
+	//Check whether the query was successful or not
+	if($result) {
+		if(mysql_num_rows($result) == 1) {
+			//Login Successful
+			session_regenerate_id();
+			$member = mysql_fetch_assoc($result);
+			$_SESSION['SESS_ROLLNUM'] = $roll;
+			$_SESSION['SESS_NAME'] = $member['Name'];
+			$_SESSION['SESS_DEPT'] = $member['Dept'];
+			session_write_close();
+			header("location: member-index.php");
+			exit();
+		}else {
+			//Login failed
+			header("location: login-failed.php");
+			exit();
+		}
+	}else {
+		die("Query failed");
+	}
+?>
